99d21eafd14ecb6ae1b86c1f607b3ee5609f0a47
bigulises <bigulises@hotmail.es>
2019-04-02 14:36:58 -0700

    commit Fab

---
+++ src/pruebaunitariaj/PruebaUnitariaJ.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaunitariaj;
+
+/**
+ *
+ * @author bigul
+ */
+public class PruebaUnitariaJ {
+
+    
+    public static void main(String[] args) {
+        
+    }
+ 
+    public int suma(int n1, int n2){
+        return n1+n2;
+    }
+    
+}
